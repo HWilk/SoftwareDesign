f17655e6de76e6945d7877ea3988b6f43cedc580
Hannah Wilk <hannnah.wilk@students.olin.edu>
2014-02-16 22:29:50 -0500

    added hw4

---
+++ hw4/random_art.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Feb 14 21:47:43 2014
+
+@author: hannah
+"""
+
+# you do not have to use these particular modules, but they may help
+from random import randint
+import Image
+
+def build_random_function(min_depth, max_depth):
+    """ Takes two inputs, min_depth(minimum amount of nesting) and max_depth(
+    specidfies maximum amount of nesting. Should use recurssion
+    
+        Pulls from 5 different functions, need input between 1 and -1, output 1 and -1
+        add two more of your own functions that do said above"""
+
+    # your code goes here
+
+def evaluate_random_function(f, x, y):
+    # your doc string goes here
+
+    # your code goes here
+
+def remap_interval(val, input_interval_start, input_interval_end, output_interval_start, output_interval_end):
+    """ Maps the input value that is in the interval [input_interval_start, input_interval_end]
+        to the output interval [output_interval_start, output_interval_end].  The mapping
+        is an affine one (i.e. output = input*c + b).
+    
+        TODO: please fill out the rest of this docstring
+    """
+    # your code goes here
